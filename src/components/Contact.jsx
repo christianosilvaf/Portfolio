f9ad54d4819d0a48e603060a0d89ec2cedd3dfa9
christianosilvaf <christianosilvaf@gmail.com>
2023-09-07 23:10:58 -0500

    solo plus colores al contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,34 +19,35 @@ const Contact = ({handleContact,setHandleContact}) => {
 
 return (
     <section className={`bg-mywhite w-[500px] h-screen  p-5 shadow-lg rounded-lg font-Inter top-0 right-0 translate-x-0 fixed transition-all
-                        ${handleContact ? "translate-x-0" : "translate-x-[500px]"} transition-transform duration-700 max-[400px]:w-[350px]`}>
-        <button onClick={()=>setHandleContact(false)} className='ml-[420px] p-2 rounded-md bg-black w-[30px] aspect-square] text-center text-mywhite'>
+                        ${handleContact ? "translate-x-0" : "translate-x-[500px]"} transition-transform duration-700 max-[400px]:w-[350px] backdrop-blur-3xl 
+                        shadow-[inset_0_35px_60px_-15px_rgba(1,195,141,1)] text-palegreen font-Inter`}>
+        <button onClick={()=>setHandleContact(false)} className='ml-[420px] p-2 rounded-md bg-black w-[30px] aspect-square] text-center text-mywhite bg-bluemarin border-2 border-white'>
             X
         </button>
-        <div className='text-center my-2 p-2'>Contact me:</div>
+        <div className='text-center my-2 p-2 font-Syne font-bold text-[25px] '>Contact me:</div>
         
         <form onSubmit={handleSubmit(submit)} className='flex flex-col justify-between gap-3 items-center w-[400px] mx-auto' action="">
             <label className='flex justify-between gap-2 w-[300px]' htmlFor="">
                 Name:
-                <input type="text" id="name" {...register("name")} className=' rounded-md shadow-sm shadow-sea' required/>
+                <input type="text" id="name" {...register("name")} className=' rounded-md shadow-sm shadow-sea bg-gray text-white p-1' required/>
             </label>
 
             <label htmlFor="" className='flex justify-between gap-2 w-[300px]'>
                 Email:
-                <input type="email" id="email" {...register("email")} className=' rounded-md shadow-sm shadow-sea' required/>
+                <input type="email" id="email" {...register("email")} className=' rounded-md shadow-sm shadow-sea bg-gray text-white p-1' required/>
             </label>
 
             <label htmlFor="" className='flex justify-between gap-2 w-[300px]'>
                 Subject:
-                <input type="text" id="subject" {...register("subject")}  className=' rounded-md shadow-sm shadow-sea' required/>
+                <input type="text" id="subject" {...register("subject")}  className=' rounded-md shadow-sm shadow-sea bg-gray text-white p-1' required/>
             </label>
 
             <label htmlFor="" className='flex  flex-col justify-between gap-2 w-[300px]'>
                 Message:
-                <input type="text" id="message" {...register("message")} className='h-[300px] w-[350px] rounded-md shadow-sm shadow-sea text-justify' required/>
+                <input type="text" id="message" {...register("message")} className='h-[300px] w-[350px] rounded-md shadow-sm shadow-sea text-justify bg-gray text-white p-1' required/>
             </label>
 
-            <input type="submit" value="Submit" {...register("Submit")} className='w-[100px] rounded-lg bg-myblue p-2 mx-auto' />
+            <input type="submit" value="Submit" {...register("Submit")} className='w-[100px] rounded-lg bg-myblue p-2 mx-auto cursor-pointer bg-bluemarin border-2 border-white' />
         </form>
 
     </section>
